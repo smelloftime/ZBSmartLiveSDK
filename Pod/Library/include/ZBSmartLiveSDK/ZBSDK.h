dd2a86e0e64db397f51d4e860e3ece185cfb4090
LipYoung <447209844qq@gmail.com>
2016-07-19 15:23:49 +0800

    修改了文件目录

---
+++ Pod/Library/include/ZBSmartLiveSDK/ZBSDK.h
@@ -0,0 +1,16 @@
+//
+//  ZBSDK.h
+//  ZBSDK
+//
+//  Created by lip on 16/7/19.
+//  Copyright © 2016年 ZhiBo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZBSDK : NSObject
++ (void)test;
+
++ (void)update;
+
+@end
