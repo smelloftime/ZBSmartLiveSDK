c7b80c50aaed3265d5b610208b082ccef825ba88
LipYoung <447209844qq@gmail.com>
2016-09-06 16:46:56 +0800

    更新了0.3.3修改了 禁言的BUG

--- Pod/Library/include/ZBSmartLiveSDK/ZBChatroom.h
+++ Pod/Library/include/ZBSmartLiveSDK/ZBChatroom.h
@@ -24,7 +24,7 @@
  *  @note 只有主播才能顺利使用该接口
  *
  *  @param userIdentity 被禁言用户的唯一标识
- *  @param disableTime  禁言时长,单位秒
+ *  @param disableTime  禁言时长,单位秒,如果设置为 0 表示永久禁言
  *  @param completion   响应结果,error 为空时,respondData 才会有数据,如果error有值则 respondData 为空
  */
 - (void)disableChatroomUserIdentity:(NSString *)userIdentity sendMessageWithTime:(NSUInteger)disableTime completion:(void(^)(id respondData, NSError *error))completion;
