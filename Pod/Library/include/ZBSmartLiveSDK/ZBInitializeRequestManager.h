1141cf831f205d0c029395abdf1747a39318c1c7
LipYoung <447209844qq@gmail.com>
2016-10-17 15:46:22 +0800

    添加了注释

--- Pod/Library/include/ZBSmartLiveSDK/ZBInitializeRequestManager.h
+++ Pod/Library/include/ZBSmartLiveSDK/ZBInitializeRequestManager.h
@@ -12,11 +12,13 @@ typedef void(^RequestSuccessCallBack)(id data);
 typedef void(^RequestFailCallBack)(NSError *fail);
 
 @interface ZBInitializeRequestManager : NSObject
-
+/// 发送获取智播云配置请求
 + (void)sendGetConfigRequestSuccess:(RequestSuccessCallBack)success fail:(RequestFailCallBack)fail;
 
+/// 发起获取最新智播云配置版本号请求
 + (void)sendGetConfigVersionSuccess:(RequestSuccessCallBack)success fail:(RequestFailCallBack)fail;
 
+/// 发起获取和更新智播云授权信息请求
 + (void)sendGetAndUpdataUserAuthenticityRequestWithTicket:(NSString *)ticket success:(RequestSuccessCallBack)success fail:(RequestFailCallBack)fail;
 
 /// 发起获取商务服务器授权信息请求
