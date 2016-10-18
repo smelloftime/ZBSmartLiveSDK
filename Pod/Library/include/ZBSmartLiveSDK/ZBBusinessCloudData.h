00d07a1aedee0b950f823caf5850cb7e9e538479
LipYoung <447209844qq@gmail.com>
2016-10-18 14:58:46 +0800

    增加了商务服务器接口调用

--- Pod/Library/include/ZBSmartLiveSDK/ZBBusinessCloudData.h
+++ Pod/Library/include/ZBSmartLiveSDK/ZBBusinessCloudData.h
@@ -8,9 +8,9 @@
 
 #import <Foundation/Foundation.h>
 
-typedef NS_ENUM(NSInteger, GetFansAndAttentionListType) {
-    GetFansAndAttentionListTypeFans,
-    GetFansAndAttentionListTypeAttention
+typedef NS_ENUM(NSInteger, ZBFollowActionType) {
+    ZBFollowActionTypeFans = 0, ///< 粉丝
+    ZBFollowActionTypeFollow ///< 关注
 };
 
 typedef NS_ENUM(NSInteger, ZBFollowType) {
@@ -21,16 +21,35 @@ typedef NS_ENUM(NSInteger, ZBFollowType) {
 
 @interface ZBBusinessCloudData : NSObject
 
-+ (void)zb_GetBusinessCloudDataFansAndAttentionListRequestWithType:(GetFansAndAttentionListType)type uname:(NSString *)uname success:(void(^)(id data))success fail:(void(^)(NSError *fail))fail;
-
-+ (void)zb_sendFollowRequestWithAction:(ZBFollowType)followAction usid:(NSString *)usid success:(void(^)(id data))success fail:(void(^)(NSError *fail))fail;
-
-+ (void)zb_GetUserInfoWithUsid:(NSArray *)usid success:(void(^)(id data))success fail:(void(^)(NSError *fail))fail;
-
-/** 这里实现三个接口
- 
- SDK内部实现 授权接口
- 
- TS内查找页面 入口进入时 更新用户信息 如果没有网络 或者网络不佳 就不能正常使用 直播 功能
+/**
+ *  获取商务服务器上的关注清单信息
+ *
+ *  @param type           查询类型
+ *  @param uname          搜索关键字,可以为空,如果为空表示不搜索,显示所有用户信息
+ *  @param businessUserID 商务服务器提供的用户唯一标识.从服务器`usid`映射
+ *  @param pageNumber     查询分页页码,由服务器提供
+ *  @param success        查询成功后的回调数据
+ *  @param fail           查询错误信息
  */
++ (void)zb_GetBusinessCloudDataFansFollowListRequestWithType:(ZBFollowActionType)type uname:(NSString *)uname businessUserID:(NSString *)businessUserID pageNumber:(NSUInteger)pageNumber success:(void(^)(id data))success fail:(void(^)(NSError *fail))fail;
+
+/**
+ *  对商务服务器指定用户发起关注操作
+ *
+ *  @param followAction   关注操作
+ *  @param businessUserID 商务服务器提供的用户唯一表示,从服务器`usid`映射
+ *  @param success        查询成功后的回调数据
+ *  @param fail           查询错误的信息
+ */
++ (void)zb_sendFollowRequestWithAction:(ZBFollowType)followAction businessUserID:(NSString *)businessUserID success:(void(^)(id data))success fail:(void(^)(NSError *fail))fail;
+
+/**
+ *  获取商务服务器对应的商务服务器唯一标识用户的用户信息
+ *
+ *  @param businessUserIDArray  多个商务服务器提供的用户唯一标识用逗号分隔的组成的字符串,从服务器`usid`映射
+ *  @param success              查询成功后的回调数据
+ *  @param fail                 查询错误的信息
+ */
++ (void)zb_GetUserInfoWithBusinessUserIDArray:(NSString *)businessUserIDArray success:(void(^)(id data))success fail:(void(^)(NSError *fail))fail;
+
 @end
--- Pod/Library/include/ZBSmartLiveSDK/ZBBusinessCloudData.m
+++ Pod/Library/include/ZBSmartLiveSDK/ZBBusinessCloudData.m
@@ -7,7 +7,75 @@
 //
 
 #import "ZBBusinessCloudData.h"
+#import "ZBErrorCode.h"
+#import "ZBURLPath.h"
+#import "ZBApplicationCenter.h"
+#import "ZBHttpRequestManager.h"
 
 @implementation ZBBusinessCloudData
+
++ (void)zb_GetBusinessCloudDataFansFollowListRequestWithType:(ZBFollowActionType)type uname:(NSString *)uname businessUserID:(NSString *)businessUserID pageNumber:(NSUInteger)pageNumber success:(void (^)(id))success fail:(void (^)(NSError *))fail {
+    if (businessUserID == nil || [businessUserID  length] <= 0 || pageNumber == 0) {
+        if (fail) {
+            fail([ZBErrorCode errorCreateWithErrorCode:ZBErrorCodeStatusEmptyParameter]);
+        }
+        return;
+    }
+    NSDictionary *businessAuthInfo = [ZBApplicationCenter defaultCenter].businessAuthInfo;
+    if (businessAuthInfo == nil) {
+        if (fail) {
+            fail([ZBErrorCode errorCreateWithErrorCode:ZBErrorCodeStatusUnInitialize]);
+        }
+        return;
+    }
+    NSArray *typeArr = @[@"fans", @"follow"];
+    if (uname == nil || [uname length] <= 0) {
+        uname = nil;
+    }
+    NSMutableDictionary *parameters = [NSMutableDictionary dictionaryWithDictionary:@{@"type":typeArr[type], @"usid":businessUserID, @"p":@(pageNumber)}];
+    [parameters addEntriesFromDictionary:businessAuthInfo];
+    if (uname) {
+        [parameters setObject:uname forKey:@"uname"];
+    }
+    [ZBHttpRequestManager sendBusinessHttpRequestWithAPI:[ZBURLPath pathFromBusinessFollowList] arguments:parameters header:nil successCallback:success failCallback:fail];
+}
+
++ (void)zb_sendFollowRequestWithAction:(ZBFollowType)followAction businessUserID:(NSString *)businessUserID success:(void (^)(id))success fail:(void (^)(NSError *))fail{
+    if (businessUserID == nil || [businessUserID  length] <= 0) {
+        if (fail) {
+            fail([ZBErrorCode errorCreateWithErrorCode:ZBErrorCodeStatusEmptyParameter]);
+        }
+        return;
+    }
+    NSDictionary *businessAuthInfo = [ZBApplicationCenter defaultCenter].businessAuthInfo;
+    if (businessAuthInfo == nil) {
+        if (fail) {
+            fail([ZBErrorCode errorCreateWithErrorCode:ZBErrorCodeStatusUnInitialize]);
+        }
+        return;
+    }
+    NSMutableDictionary *parameters = [NSMutableDictionary dictionaryWithDictionary:@{@"usid":businessUserID, @"action":@(followAction)}];
+    [parameters addEntriesFromDictionary:businessAuthInfo];
+    [ZBHttpRequestManager sendBusinessHttpRequestWithAPI:[ZBURLPath pathFromBusinessFollowAction] arguments:parameters header:nil successCallback:success failCallback:fail];
+}
+
++ (void)zb_GetUserInfoWithBusinessUserIDArray:(NSString *)businessUserIDArray success:(void (^)(id))success fail:(void (^)(NSError *))fail {
+    if (businessUserIDArray == nil || [businessUserIDArray  length] <= 0) {
+        if (fail) {
+            fail([ZBErrorCode errorCreateWithErrorCode:ZBErrorCodeStatusEmptyParameter]);
+        }
+        return;
+    }
+    NSDictionary *businessAuthInfo = [ZBApplicationCenter defaultCenter].businessAuthInfo;
+    if (businessAuthInfo == nil) {
+        if (fail) {
+            fail([ZBErrorCode errorCreateWithErrorCode:ZBErrorCodeStatusUnInitialize]);
+        }
+        return;
+    }
+    NSMutableDictionary *parameters = [NSMutableDictionary dictionaryWithDictionary:@{@"usid":businessUserIDArray}];
+    [parameters addEntriesFromDictionary:businessAuthInfo];
+    [ZBHttpRequestManager sendBusinessHttpRequestWithAPI:[ZBURLPath pathFromBusinessUserInfo] arguments:parameters header:nil successCallback:success failCallback:fail];
+}
 
 @end
